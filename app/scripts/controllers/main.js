7967406fb9012c9368b3635a6f1e7a48af9f93f4
Rajnish <dashora.rajnish@gmail.com>
2014-08-29 17:31:09 +0530

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modalTestAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the modalTestAppApp
+ */
+angular.module('modalTestAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
